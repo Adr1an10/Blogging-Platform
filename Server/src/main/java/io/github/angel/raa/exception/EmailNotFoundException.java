07dda41305c53c4557662f0dc632bcc7d836f751
Angel Aguero <angelagueror23@gmail.com>
2025-05-05 17:43:51 -0400

    Agregar clases de respuesta de error y excepción para el manejo de correos no encontrados

---
+++ Server/src/main/java/io/github/angel/raa/exception/EmailNotFoundException.java
@@ -0,0 +1,7 @@
+package io.github.angel.raa.exception;
+
+public class EmailNotFoundException extends RuntimeException {
+  public EmailNotFoundException(String message) {
+    super(message);
+  }
+}
