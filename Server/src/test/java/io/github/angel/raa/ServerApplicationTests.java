7016268bdb5ae8c31dc99470a941098b6f196964
Angel Aguero <angelagueror23@gmail.com>
2025-05-04 22:55:22 -0400

    Initial commit

---
+++ Server/src/test/java/io/github/angel/raa/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.angel.raa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
