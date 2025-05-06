4d609904860991e6b5ec28c239bb4fc7afdd3447
Angel Aguero <angelagueror23@gmail.com>
2025-05-05 22:26:01 -0400

    feat: Añadir funcionalidad para generar slugs en Java
    - Agregar dependencia Slugify para generar slugs de forma sencilla.
    - Implementar función personalizada para generar slugs sin dependencias externas.
    - Incluir ejemplos de uso en la clase principal.

--- Server/src/main/java/io/github/angel/raa/utils/Slugify.java
+++ Server/src/main/java/io/github/angel/raa/utils/Slugify.java
@@ -11,12 +11,19 @@ import java.util.regex.Pattern;
  */
 public class Slugify {
     private static final Pattern NON_LATIN = Pattern.compile("[^\\w-]");
-    private static final Pattern SEPARATORS = Pattern.compile("[\\s\\p{Punct}&amp;&amp;[^-]]");
+    private static final Pattern SEPARATORS = Pattern.compile("[\\s\\p{Punct}&&[^-]]");
     public static @NotNull String slugify(@NotNull String text) {
         String noseparators = SEPARATORS.matcher(text).replaceAll("-");
         String normalized = Normalizer.normalize(noseparators, Normalizer.Form.NFD);
         String slug = NON_LATIN.matcher(normalized).replaceAll("");
-        return slug.toLowerCase(Locale.ENGLISH).replaceAll("-{2,}", "-").replaceAll("^-|-$", "");
+        return slug.toLowerCase().replaceAll("-{2,}", "-").replaceAll("^-|-$", "");
+
+    }
+
+    public static void main(String[] args) {
+        String text = "Léte ou es-tu?";
+        String slug = Slugify.slugify(text);
+        System.out.println("Hola Mundo " + slug); // Output: lete-ou-es-tu
 
     }
 }
